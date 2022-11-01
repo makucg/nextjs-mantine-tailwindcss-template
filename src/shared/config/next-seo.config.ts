86e62d17f1e081bd56f5e776f887f1f78ff5aa8d
DiegoMN <chengokuu@gmail.com>
2022-11-01 14:52:01 +0100

    Initial commit

--- src/shared/config/next-seo.config.ts
+++ src/shared/config/next-seo.config.ts
@@ -1,29 +1,29 @@
 import { DefaultSeoProps } from 'next-seo';
 
 export const SEOConfig: DefaultSeoProps = {
-  titleTemplate: '%s | nextjs-mantine-template',
-  defaultTitle: 'nextjs-mantine-template',
-  description: 'A template for Next.js with Mantine UI',
-  canonical: 'https://nextjs-mantine-template.deploy.cnoside.dev',
-  additionalLinkTags: [
-    {
-      rel: 'icon',
-      href: '/favicon.ico'
-    }
-  ],
-  openGraph: {
-    url: 'https://nextjs-mantine-template.deploy.cnoside.dev',
-    title: 'nextjs-mantine-template',
-    type: 'website',
-    site_name: 'nextjs-mantine-template',
-    locale: 'en_SG',
-    images: [
-      {
-        url: 'https://dev-cnoside.sgp1.digitaloceanspaces.com/shared/images/nextjs-mantine-template.og-image.jpg',
-        alt: 'nextjs-mantine-template og:image',
-        width: 1280,
-        height: 640
-      }
-    ]
-  }
+	titleTemplate: '%s | nextjs-mantine-tailwindcss-template',
+	defaultTitle: 'nextjs-mantine-tailwindcss-template',
+	description: 'A template for Next.js with Mantine UI and TailwindCSS',
+	canonical: 'https://nextjs-mantine-template.deploy.cnoside.dev',
+	additionalLinkTags: [
+		{
+			rel: 'icon',
+			href: '/favicon.ico',
+		},
+	],
+	openGraph: {
+		url: 'https://nextjs-mantine-template.deploy.cnoside.dev',
+		title: 'nextjs-mantine-tailwindcss-template',
+		type: 'website',
+		site_name: 'nextjs-mantine-tailwindcss-template',
+		locale: 'es_ES',
+		images: [
+			{
+				url: 'https://dev-cnoside.sgp1.digitaloceanspaces.com/shared/images/nextjs-mantine-template.og-image.jpg',
+				alt: 'nextjs-mantine-tailwindcss-template og:image',
+				width: 1280,
+				height: 640,
+			},
+		],
+	},
 };
