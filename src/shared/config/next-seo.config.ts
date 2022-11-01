6328f3ee3bcecdb41783a8cb37f5bb455421cfbc
DiegoMN <chengokuu@gmail.com>
2022-11-01 17:17:41 +0100

    Update next-seo.config.ts

--- src/shared/config/next-seo.config.ts
+++ src/shared/config/next-seo.config.ts
@@ -4,7 +4,7 @@ export const SEOConfig: DefaultSeoProps = {
 	titleTemplate: '%s | nextjs-mantine-tailwindcss-template',
 	defaultTitle: 'nextjs-mantine-tailwindcss-template',
 	description: 'A template for Next.js with Mantine UI and TailwindCSS',
-	canonical: 'https://nextjs-mantine-template.deploy.cnoside.dev',
+	canonical: 'https://nextjs-mantine-tailwindcss-template.netlify.app',
 	additionalLinkTags: [
 		{
 			rel: 'icon',
@@ -12,14 +12,14 @@ export const SEOConfig: DefaultSeoProps = {
 		},
 	],
 	openGraph: {
-		url: 'https://nextjs-mantine-template.deploy.cnoside.dev',
+		url: 'https://nextjs-mantine-tailwindcss-template.netlify.app',
 		title: 'nextjs-mantine-tailwindcss-template',
 		type: 'website',
 		site_name: 'nextjs-mantine-tailwindcss-template',
 		locale: 'es_ES',
 		images: [
 			{
-				url: 'https://dev-cnoside.sgp1.digitaloceanspaces.com/shared/images/nextjs-mantine-template.og-image.jpg',
+				url: 'https://github.com/makucg/nextjs-mantine-tailwindcss-template/blob/main/src/assets/image.png',
 				alt: 'nextjs-mantine-tailwindcss-template og:image',
 				width: 1280,
 				height: 640,
